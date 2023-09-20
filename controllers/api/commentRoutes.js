b393ac3ba5f0bd7c74a059f77c3c885839872c51
Donnie Rawlings <tacobellboi@yahoo.com>
2023-09-19 19:50:53 -0500

    comment routes update

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,48 +2,45 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res) => {
-  Comment.findAll()
-  .then(dbCommentData => res.json(dbCommentData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-});
 
-router.post('/', withAuth, (req, res) => {
-  //Check for a session
-  if (req.session) {
-    Comment.create({
-      comment_text: req.body.comment_text,      
-      post_id: req.body.post_id,
-      user_id: req.session.user_id
-    })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
     });
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.sendStatus(500).send(err);
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const destroyComment =
   Comment.destroy({
     where: {
       id: req.params.id
     }
   })
-    .then(dbCommentData => {
-      if (!dbCommentData) {
-        res.status(404).json({ message: 'No comment with this id found' });
-        return;
-      }
-      res.json(dbCommentData);
-    })
-    .catch(err => {
+    return res.status(200).json(destroyComment)
+} catch (err){
+    res.status(500).json(err);
+}
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+      const updateComment = await Comment.update(req.body, {
+          where: {
+              id: req.params.id,
+          }
+      })
+      res.status(200).json(updateComment);
+  } catch (err) {
       console.log(err);
       res.status(500).json(err);
-    });
+  }
 });
 
 module.exports = router;
