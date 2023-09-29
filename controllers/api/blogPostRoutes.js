2f2b6549606e39e789a6b4b0715b9c646cc0d64a
Donnie Rawlings <tacobellboi@yahoo.com>
2023-09-28 22:33:45 -0500

    fixed casing on Post.js model

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
-cd 
+
 router.post('/', withAuth, async (req, res) => {
   try {
     const newBlogPost = await Post.create({
